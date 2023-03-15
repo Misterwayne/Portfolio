fddcde5e2b0524f98a488c0d65e64f328d100365
Malick WANE <mwane@e2r7p10.clusters.42paris.fr>
2023-03-15 19:21:14 +0100

    all the files

---
+++ front-react/src/component/Experience.jsx
@@ -0,0 +1,47 @@
+
+
+function Experience()
+{
+    return (
+        <div className="">
+
+        <div className="row">
+            <div className="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Animateur d'atelier de développement informatique niveaux collège</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Magic Maker - Paris</h6>
+                        <p class="card-text">Novembre 2020 - Juin 2021</p>
+                        <a href="https://www.magicmakers.fr/" class="card-link">Magic Maker</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Serveur de restaurant</h5>
+                        <h6 class="card-subtitle mb-2 text-muted p-3">Pain Vain Fromage, 3, Rue Geoffroy-l'Angevin - 75004 Paris</h6>
+                        <p class="card-text">Avril 2019 à juin 2019</p>
+                        <a href="http://painvinfromages.com/" class="card-link">Pain Vin Fromage</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Chef de rang</h5>
+                        <h6 class="card-subtitle mb-2 text-muted p-3">Grand Bistro De la Muette 10 Chaussé de la Muette 75016 - Paris</h6>
+                        <p class="card-text">Janvier 2018 à mars 2018</p>
+                        <div className="p1">
+                            Maintenent ferme
+                            </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+
+}
+
+export default Experience;
